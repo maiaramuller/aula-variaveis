a6337973b470f4df24951e5b9707751b6d10d72d
BurakkuBR <fabio.conceicao94@gmail.com>
2023-08-17 19:35:22 -0300

    conclusao dos exedrcicios

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(10,5)
 */
 
 /* 2. Exercício:
-console.log(20, 10, 10)
+console.log(10, 10, 10)
 */
 
 /* 3. Exercício:
@@ -54,11 +54,27 @@ segunda variável: salarioDiario
 
 //2.
 //a
-const dancar = prompt("Você gosta de dançar?");
+/*const dancar = prompt("Você gosta de dançar?");
 console.log(dancar);
 
 const dormir = prompt("Você gosta de dormir?");
 console.log(dormir);
 
 const ler = prompt("Você gosta de ler?");
-console.log(ler);
+console.log(ler);*/
+
+//3.
+
+/*let a = 10;
+let b = 25;
+
+// Aqui faremos uma lógica para trocar os valores
+
+c = a;
+a = b;
+b = c;
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a); // O novo valor de a é 25
+console.log("O novo valor de b é", b); // O novo valor de b é 10 
+*/
