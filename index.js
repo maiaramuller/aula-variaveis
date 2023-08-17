c2c434691aacf486fe35cde7abcee455577939e8
BurakkuBR <fabio.conceicao94@gmail.com>
2023-08-16 21:54:59 -0300

    ultimas aterações

---
+++ index.js
@@ -0,0 +1,64 @@
+// Exercícios de interpretação
+
+/* 1. Exercício:
+console.log(10,5)
+*/
+
+/* 2. Exercício:
+console.log(20, 10, 10)
+*/
+
+/* 3. Exercício:
+primeira variável: horasTrabalhadasDia
+segunda variável: salarioDiario
+*/
+
+//Exercícios de escrita de código
+
+//1.
+
+//a
+//let nome
+
+//b
+//let idade
+
+//c
+//typeof nome
+//typeof idade
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+//d.  como não foi atribuido nenhum valor para a variável, nos foi retornado "undefined".
+
+//e
+
+//let nome = prompt("Qua é o seu nome?");
+//let idade = prompt("Qual é a sua idade?");
+
+//console.log(nome, idade);
+//f
+//typeof nome;
+//typeof idade;
+//agora como foi atribuido um valor a variável, foi retornado o tipo "string" que era o esperado.
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+//g
+//let nome = prompt("Qua é o seu nome?");
+//let idade = prompt("Qual é a sua idade?");
+
+//console.log("Olá", nome, "você tem", idade, "anos");
+
+//2.
+//a
+const dancar = prompt("Você gosta de dançar?");
+console.log(dancar);
+
+const dormir = prompt("Você gosta de dormir?");
+console.log(dormir);
+
+const ler = prompt("Você gosta de ler?");
+console.log(ler);
